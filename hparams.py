7305b3e67d223e9ed84e5cd12016ff06d3d93aec
Fazry <lhuqita.fazry@gmail.com>
2023-01-22 14:17:25 +0900

    d

--- hparams.py
+++ hparams.py
@@ -5,7 +5,7 @@ from text import symbols
 def create_hparams(hparams_string=None, verbose=False):
     """Create model hyperparameters. Parse nondefault from given string."""
 
-    hparams = tf.contrib.training.HParams(
+    hparams = dict(
         ################################
         # Experiment Parameters        #
         ################################
@@ -85,11 +85,11 @@ def create_hparams(hparams_string=None, verbose=False):
         mask_padding=True  # set model's padded outputs to padded values
     )
 
-    if hparams_string:
-        tf.logging.info('Parsing command line hparams: %s', hparams_string)
-        hparams.parse(hparams_string)
+    #if hparams_string:
+    #    tf.logging.info('Parsing command line hparams: %s', hparams_string)
+    #    hparams.parse(hparams_string)
 
-    if verbose:
-        tf.logging.info('Final parsed hparams: %s', hparams.values())
+    #if verbose:
+    #    tf.logging.info('Final parsed hparams: %s', hparams.values())
 
     return hparams
